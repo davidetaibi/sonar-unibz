e8be7973b1a0d2bee45a7bce7e442345c2145fe5
Michael Mairegger <michael.mairegger@unibz.it>
2015-09-05 13:19:15 +0200

    remove license information from file header.

--- sonar-analyzer/Sonar-Git-Analyzer/Program.cs
+++ sonar-analyzer/Sonar-Git-Analyzer/Program.cs
@@ -1,14 +1,4 @@
-﻿// -----------------------------------------------------------------------
-// <copyright file="Program.cs"
-//           project="Sonar-Git-Analyzer"
-//           company="Mairegger Michael"
-//           webpage="http://michaelmairegger.wordpress.com">
-//     Copyright © Mairegger Michael, 2015 
-//     All rights reserved
-// </copyright>
-// -----------------------------------------------------------------------
-
-namespace Sonar_Git_Analyzer
+﻿namespace Sonar_Git_Analyzer
 {
     using System;
     using System.Collections.Generic;
